ce92d2604e000ffa5b0066455e08b6754476f9f3
josvaler <jose.valerio@gmx.com>
2017-04-11 02:19:13 -0500

    Update auth0-variables.js

--- www/js/auth0-variables.js
+++ www/js/auth0-variables.js
@@ -1,3 +1,4 @@
+// You should change AUTH_CLIENT_ID y AUTH0_DOMAIN
 var AUTH0_CLIENT_ID='F3h68LE3FXa6agh2TP0z9Qc4LXiD4rEe';
 var AUTH0_CALLBACK_URL=location.href;
 var AUTH0_DOMAIN='njrr.auth0.com';
